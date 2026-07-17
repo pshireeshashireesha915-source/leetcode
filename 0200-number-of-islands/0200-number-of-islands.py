a0ff6e8cb220e30531762b5242b48d5ec0324a99
pshireeshashireesha915-source <pshireeshashireesha915@gmail.com>
2026-07-17 12:08:22 +0530

    Time: 235 ms (81.88%), Space: 21.6 MB (47.95%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,37 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row=len(grid)
+        column=len(grid[0])
+        visited=[]
+        for i in range(row):
+            visited.append([0]*column)
+        count = 0
+        for i in range(row):
+            for j in range(column):
+                if grid[i][j] == "1" and  visited[i][j]==0:
+                    self.bfs(grid,visited,i,j)
+                    count+=1
+        return count
+    def bfs(self,grid,visited,i,j):
+        row=len(grid)
+        column=len(grid[0])
+        q=deque()
+        q.append((i,j))
+        visited[i][j]=1
+        while q:
+            i,j=q.popleft()
+            visited[i][j]=1
+            if i-1>=0 and grid[i-1][j] == "1" and visited[i-1][j]==0:
+                q.append((i-1,j))
+                visited[i-1][j]=1
+            if j+1<column and grid[i][j+1] == "1" and visited[i][j+1]==0:
+                q.append((i,j+1))
+                visited[i][j+1]=1
+            if i+1<row and grid[i+1][j] == "1" and visited[i+1][j]==0:
+                q.append((i+1,j))
+                visited[i+1][j]=1
+            if j-1>=0 and grid[i][j-1] == "1" and visited[i][j-1]==0:
+                q.append((i,j-1))
+                visited[i][j-1]
+            
+
